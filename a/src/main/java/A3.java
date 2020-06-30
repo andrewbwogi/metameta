63a6b65a19f592819a0184b73c1d5d36795e3e1f
Andrew Bwogi <bwogi.andrew@gmail.com>
2020-06-30 17:46:57 +0200

    add test cases

---
+++ a/src/main/java/A3.java
@@ -0,0 +1,12 @@
+public class A3
+{
+    public int method(int x) {
+        int i = 0;
+        return x + i;
+    }
+
+    public void method() {
+        int i = 0;
+        int x = 1;
+    }
+}
---
+++ a/src/main/java/A4.java
@@ -0,0 +1,12 @@
+public class A4
+{
+    public int method(int x) {
+        int i = 0;
+        return x + i;
+    }
+
+    public void otherMethod() {
+        int i = 0;
+        int x = 1;
+    }
+}
